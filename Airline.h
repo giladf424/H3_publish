fed4d48123241173df287cd767a020218ba92dec
giladf424 <giladf424@gmail.com>
2024-03-02 11:56:39 +0200

    Add project files.

---
+++ Airline.h
@@ -0,0 +1,38 @@
+#ifndef __COMP__
+#define __COMP__
+
+#include "Flight.h"
+#include "AirportManager.h"
+
+typedef enum {
+	eNotSorted, eSrcCode, eDstCode, eDate, eNofSortTypes
+} eSortType;
+
+static const char* SortTypeStr[eNofSortTypes]
+= { "NotSorted", "SrcCode", "DstCode", "Date"};
+
+
+typedef struct
+{
+	char*		name;
+	int			flightCount;
+	Flight**	flightArr;
+	int			planeCount;
+	Plane*		planeArr;
+	eSortType type;
+}Airline;
+
+void	initAirline(Airline* pComp);
+int		addFlight(Airline* pComp,const AirportManager* pManager);
+int		addPlane(Airline* pComp);
+Plane*	FindAPlane(Airline* pComp);
+void	printCompany(const Airline* pComp);
+void	printFlightArr(Flight** arr, int size);
+void	printPlanesArr(Plane* arr,int size);
+void	doPrintFlightsWithPlaneType(const Airline* pComp);
+void	freeFlightArr(Flight** arr, int size);
+void	freePlanes(Plane* arr, int size);
+void	freeCompany(Airline* pComp);
+
+#endif
+
